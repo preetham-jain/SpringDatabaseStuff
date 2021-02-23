887663eae22981a153845b9b125618f3cb13602b
Preetham Jain <preethamjain@Preethams-MacBook-Pro.local>
2021-02-23 14:13:14 +0530

    first commit

---
+++ src/test/java/com/example/springDataExample/SpringDataExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDataExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
