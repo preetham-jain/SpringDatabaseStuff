398704702b773608c1791ddb4d7a6ab925aa5ac1
Preetham Jain <preethamjain@Preethams-MacBook-Pro.local>
2021-02-23 18:28:33 +0530

    redis commit

--- src/main/java/com/example/springDataExample/entity/Employee.java
+++ src/main/java/com/example/springDataExample/entity/Employee.java
@@ -2,14 +2,14 @@ package com.example.springDataExample.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
-@Document
+@RedisHash
 @Getter
 @Setter
 public class Employee {
-    @MongoId
+    @Id
     private Long id;
     private String name;
     private String departmentName;
